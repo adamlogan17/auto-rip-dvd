fc1c13343350a8dc126b73e571fa24f7fa2e6d28
Adam Logan <adamlogan42@gmail.com>
2025-07-03 19:55:07 +0100

    Refactored user input

--- auto-rip.py
+++ auto-rip.py
@@ -26,7 +26,9 @@ def dvd_detected(drive_path):
     :param drive_path: The path to the disc drive
     :return: True if a DVD is detected, False otherwise.
     """
-    return os.path.exists(drive_path) and os.path.isdir(drive_path)
+    print(os.listdir(drive_path))
+    print(drive_path)
+    return os.path.exists(drive_path) and os.path.isdir(drive_path) and os.listdir(drive_path) != []
 
 def parse_makemkv_title_info(makemkv_info):
     """
@@ -277,61 +279,80 @@ def rip_dvd_title(iso_filename, mp4_output_folder, mkv_output_folder, title_id, 
     if handbrake_started and os.path.exists(mp4_name):
         print(f"MP4 conversion completed: {mp4_name}")
 
-# NOTE: Need to split this function up, as it is too large
-# NOTE: Add a check to see if the rip was successful and if so call store_media_info
-def main(output_folders):
-    # This is modified, if there is a tv show and therefore needs to be copied to ensure that thr argument is not modified
-    out_folders = copy.deepcopy(output_folders)
-    titles_to_rip = [] # NOTE: rename this variable, as it no longer holds the title id
+def console_user_input():
     dvd_title = str(input('Title of DVD (Movie/TV Series Name): ')).strip()
-    media_info = {}
-    iso_name = ''
-    tv_show = False
 
     special_feature_input = str(input('Is this a special features disc? y/n: ')).strip().lower()
     special_feature = special_feature_input == 'y'
 
-    # Handles DVDs with multiple episodes
+
     tv_show_input = str((input('Is this a TV show? y/n: '))).strip().lower()
-    if tv_show_input == 'y':
-        tv_show = True
+    
+    if tv_show_input == 'y' and not special_feature:
+        season_number = int(input(f'Enter the season number for this disc: ').strip())
+        first_episode = int(input(f'Enter the first episode number: ').strip())
+        num_episodes = int((input('Enter the number of episodes on the disc: ')).strip())
+    elif tv_show_input == 'y':
+        season_number = 0
+        first_episode = 0
+        num_episodes = 0
+
+    return {
+        'dvd_title': dvd_title,
+        'tv_show': {
+            'season_number': season_number,
+            'first_episode': first_episode,
+            'num_episodes': num_episodes
+        },
+        'special_feature': special_feature
+    }
+
+# NOTE: Need to split this function up, as it is too large
+# NOTE: Add a check to see if the rip was successful and if so call store_media_info
+def main(output_folders, user_config):
+    # This is modified, if there is a tv show and therefore needs to be copied to ensure that thr argument is not modified
+    out_folders = copy.deepcopy(output_folders)
+    titles_to_rip = [] # NOTE: rename this variable, as it no longer holds the title id
+    dvd_title = user_config['dvd_title']
+    special_feature = user_config['special_feature']
+    tv_show_info = user_config['tv_show']
+
+    media_info = {}
+    iso_name = ''
+
+    # NOTE: Maybe make a 'tv-show' function. This means you could pass special features as  {'season': 0, 'first_episode': 0, 'num_episodes': 0}
+    # NOTE: This would clean up this messy if statement handling, as it checks for special features twice
+    # Handles DVDs with multiple episodes
+    if tv_show_info:
         # Get the media info from TMDB
         media_info = tmdb_tv_info(dvd_title)
-        number_of_seasons = len(media_info['seasons'])
-        print(f"This show has {number_of_seasons} season(s).")
 
-        # Get user input for season and episode information
-        season_number = 1
-        if number_of_seasons > 1:
-            season_number = int(input(f'Enter the season number for this disc (1-{number_of_seasons}): ').strip())
+        # Special features should be treated as Season 0, due to how plex and jellyfin handle special features
+        season_number = 0 if special_feature else tv_show_info['season_number']
+        # Adds a leading zero to the season and episode numbers for formatting (both Plex and Jellyfin use this format)
+        formatted_season = f"{season_number:02d}"
 
         # Special features, need to be handle here, so it can be added as 'Season 0', within the TV folder structure
         # This is how both plex and Jellyfin handle special features
         # Setting the 'first_episode' and 'num_episode' to 0, prevents the for loop below from entering
-        if special_feature:
-            season_number = 0
-            first_episode = 0
-            num_episodes = 0
-        else:
-            first_episode = int(input(f'Enter the first episode number: ').strip())
-            num_episodes = int((input('Enter the number of episodes on the disc: ')).strip())
+        if not special_feature:
+            first_episode = tv_show_info['first_episode']
+            num_episodes = tv_show_info['num_episodes']
+
             # 1 is subtracted, is because the 'first episode' is included in the count
             iso_name = f"{dvd_title} s{formatted_season}e{first_episode:02d} - e{((first_episode+num_episodes)-1):02d}"
 
-        # Adds a leading zero to the season and episode numbers for formatting (both Plex and Jellyfin use this format)
-        formatted_season = f"{season_number:02d}"
-
-        # Get the runtime and episode name for each episode on the disc
-        for episode_number in range(first_episode, first_episode + num_episodes):
-            # Need to subtract 1 to episode_number as the list is 0-indexed
-            episode_info = media_info['seasons'][season_number-1]['episodes'][episode_number-1]
-            formatted_episode = f"{episode_number:02d}"
-            titles_to_rip.append(
-                {
-                    'file_name': f"{dvd_title} - s{formatted_season}e{formatted_episode} - {episode_info['episode_name']}",
-                    'expected_runtime': episode_info['runtime']
-                }
-            )
+            # Get the runtime and episode name for each episode on the disc
+            for episode_number in range(first_episode, first_episode + num_episodes):
+                # Need to subtract 1 to episode_number as the list is 0-indexed
+                episode_info = media_info['seasons'][season_number-1]['episodes'][episode_number-1]
+                formatted_episode = f"{episode_number:02d}"
+                titles_to_rip.append(
+                    {
+                        'file_name': f"{dvd_title} - s{formatted_season}e{formatted_episode} - {episode_info['episode_name']}",
+                        'expected_runtime': episode_info['runtime']
+                    }
+                )
 
         # Create the path for the tv show and seasons, based on the file structure provided by Plex and Jellyfin
         season_folder_name = f"Season {formatted_season}"
@@ -388,7 +409,7 @@ def main(output_folders):
             print(titles_to_rip)
 
         previous_title_ids = [] # Used to prevent the same title_id being used for multiple titles
-        threshold = 4 if tv_show else 10 # Threshold needs to be shorter, to allow for the specific episode to be found
+        threshold = 4 if tv_show_info else 10 # Threshold needs to be shorter, to allow for the specific episode to be found
         for title in titles_to_rip:
             title_id = -1 # Default to -1, prevents same title_id being used for multiple titles
             # As this is an int, the explicit check for None is needed. This is due to '0' being a valid title_id but it is treated as False in Python
@@ -421,6 +442,8 @@ if __name__ == '__main__':
         'iso': iso_out_dir
     }
 
+    user_input = console_user_input()
+
     while True:
         if dvd_detected(disc_drive):
-            main(output_folders)
+            main(output_folders, user_input)
