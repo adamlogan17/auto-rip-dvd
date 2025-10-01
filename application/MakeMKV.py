35c3eefc835101c691c1d92a32aa2072e52fed6d
Adam Logan <adamlogan42@gmail.com>
2025-10-01 20:37:12 +0100

    Add MakeMKV class

---
+++ application/MakeMKV.py
@@ -0,0 +1,132 @@
+import json
+import subprocess
+from VideoRipApp import VideoRipApp, TitleInfo, SeasonInformation
+from typing import List
+import os
+import re
+
+class MakeMKV(VideoRipApp):
+    def __init__(self, application_path: str):
+        super().__init__(application_path, '.mkv')
+        pass
+
+    def disc_backup(self, output_path: str, disc_name: str) -> str:
+        disc_number = 0
+        
+        output = self._get_output_filename(output_path, disc_name)
+        output = f"{output[:-len(self.file_extension)]}.iso"
+        
+        if os.path.exists(output):
+            print(f"Output file already exists, skipping extraction: {output}")
+            return output
+
+        rip_command = [
+            self.application_path,
+            'backup',
+            f"disc:{disc_number}", # is whatever is in the disc drive (may need to change index, if multiple drive). Possible solution is to use the 'file:<>' option and point to 'E:'
+            output,
+            '--noscan',
+            '--decrypt'
+        ]
+
+        try:
+            print(f"Starting MakeMKV decryption for disc:{disc_number}...")
+            subprocess.run(rip_command, check=True)
+            print(f"Decryption completed. Output saved to {output}.")
+            return output
+        except FileNotFoundError:
+            print("Error: MakeMKV not found. Please check the path to MakeMKV.")
+            return None
+        except subprocess.CalledProcessError as e:
+            print(f"Error: MakeMKV failed with error code {e.returncode}.")
+            return None
+
+    def _raw_title_info(self, iso_filename: str) -> dict:
+        info_command = [
+            self.application_path,
+            'info',
+            f"file:{iso_filename}",
+            '--robot'
+        ]
+
+        try:
+            print(f"Starting retrieving disc information using MakeMKV...")
+            disc_info = subprocess.run(info_command, check=True, text=True, capture_output=True)
+            print(f"Decryption completed. Output saved to {'output_file'}.")
+            return disc_info.stdout
+        except FileNotFoundError:
+            print("Error: MakeMKV not found. Please check the path to MakeMKV.")
+        except subprocess.CalledProcessError as e:
+            print(f"Error: MakeMKV failed with error code {e.returncode}.")
+
+    def _parse_raw_title_info(self, raw_title_info: List) -> List[TitleInfo]:
+        title_data = []
+        
+        for line in raw_title_info.splitlines():
+            processed_line = line.split(',')
+            info_title = processed_line[0].split(':')
+            info_type = info_title[0]
+            
+            if 'TINFO' in info_type:
+                title_id = int(info_title[1])
+                info = processed_line[-1].strip('"')
+                runtime = -1
+                
+                # Check if the info is a runtime in the format HH:MM or HH:MM:SS
+                if re.match(r'^\d{1,2}:\d{2}(:\d{2})?$', info):
+                    time_parts = info.split(':')
+                    if len(time_parts) == 3:
+                        hours = int(time_parts[0])
+                        minutes = int(time_parts[1])
+                        runtime = (hours * 60) + minutes
+                    elif len(time_parts) == 2:
+                        hours = int(time_parts[0])
+                        minutes = int(time_parts[1])
+                        runtime = (hours * 60) + minutes
+                
+                # Only add entries with valid runtime data
+                if runtime > 0:
+                    title_data.append(TitleInfo(title_id=title_id, runtime=runtime))
+        return title_data
+            
+    def _extract_video_file(self, iso_filename: str, title_id: int, output_path: str) -> str:
+        output = os.path.dirname(output_path)
+        current_files = os.listdir(output)
+        file_name = os.path.basename(output_path)
+
+        mkv_command = [
+            self.application_path,
+            'mkv',
+            f"file:{iso_filename}",
+            f"{title_id}",
+            output,
+            '--noscan'
+        ]
+        
+        try:
+            print(f"Starting MakeMKV decryption for disc:0...")
+            subprocess.run(mkv_command, check=True)
+            print(f"Decryption completed. Output saved to {output}.")
+        except FileNotFoundError:
+            print("Error: MakeMKV not found. Please check the path to MakeMKV.")
+        except subprocess.CalledProcessError as e:
+            print(f"Error: MakeMKV failed with error code {e.returncode}.")
+
+        updated_files = os.listdir(output)
+        new_file_name = list(set(updated_files) - set(current_files))
+        if new_file_name:
+            new_file_path = os.path.join(output, new_file_name[0])
+            mkv_name = os.path.join(output, file_name)
+            os.rename(new_file_path, mkv_name)
+
+if __name__ == "__main__":
+    app = MakeMKV("C:\\Program Files (x86)\\MakeMKV\\makemkvcon")
+    print(app.application_path)
+
+    iso_file = app.disc_backup("F:\\test", "A Few Good Men")
+
+    main_feature_title = app.get_main_feature(iso_file, 138)
+    print(f"Main feature title ID: {main_feature_title}")
+
+    app.extract_video(iso_file, main_feature_title, "F:\\test")
+
